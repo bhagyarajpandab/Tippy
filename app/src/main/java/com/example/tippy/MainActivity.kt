7821f5e552e8ffa55c6580fa12ac2673bcb0248a
bhagyarajpandab <1805291@kiit.ac.in>
2022-05-01 09:28:57 +0530

    Added Split Functionality and fixed rounding off

--- app/src/main/java/com/example/tippy/MainActivity.kt
+++ app/src/main/java/com/example/tippy/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.tippy
 
 import android.animation.ArgbEvaluator
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -9,8 +8,10 @@ import android.util.Log
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.tippy.databinding.ActivityMainBinding
+import kotlin.math.ceil
 
 private const val TAG = "MainActivity"
 private const val INIT_TIP_PERCENT = 15
@@ -24,7 +25,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvTotalAmount: TextView
     private lateinit var tvPercentLabel: TextView
     private lateinit var tvTipDescripton: TextView
-
+    private lateinit var etSplitPeople: EditText
+    private lateinit var tvSplitValue: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +39,8 @@ class MainActivity : AppCompatActivity() {
         tvTotalAmount = binding.tvTotalAmount
         tvPercentLabel = binding.tvPercentLabel
         tvTipDescripton = binding.tvTipDescripton
+        etSplitPeople = binding.etSplitPeople
+        tvSplitValue = binding.tvSplitValue
 
         tipSeekBar.progress = INIT_TIP_PERCENT
         tvPercentLabel.text = "$INIT_TIP_PERCENT"
@@ -69,6 +73,28 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+        etSplitPeople.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.i(TAG, "afterTextChanges $s")
+                computeSplit()
+            }
+
+        })
+
+    }
+
+    private fun computeSplit() {
+        if (etSplitPeople.text.isEmpty() || tvTotalAmount.text.isEmpty()) {
+            tvSplitValue.text = ""
+            return
+        }
+        val splitAmount =
+            ceil(tvTotalAmount.text.toString().toFloat() / etSplitPeople.text.toString().toInt())
+        tvSplitValue.text = splitAmount.toString()
     }
 
     private fun updateTipDescription(progress: Int) {
@@ -99,11 +125,13 @@ class MainActivity : AppCompatActivity() {
         val baseAmount = etBaseAmount.text.toString().toDouble()
         val tipPercent = tipSeekBar.progress
         //Compute tip and total
-        val tipAmount = baseAmount * tipPercent / 100
+        val tipAmount = ceil(baseAmount * tipPercent / 100)
         val totalAmount = baseAmount + tipAmount
         //Update the ui
         val formatter = "%.2f"
         tvTipAmount.text = formatter.format(tipAmount)
         tvTotalAmount.text = formatter.format(totalAmount)
+        if (etSplitPeople.text.isNotEmpty())
+            computeSplit()
     }
 }
